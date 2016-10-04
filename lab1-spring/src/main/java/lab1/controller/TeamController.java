633e1127a14fe774ea429a0afc74b5db10e3c66f
Tobias Johansson <tobias@johansson.xyz>
2016-10-04 17:52:59 +0200

    lab1: Complete part2

---
+++ lab1-spring/src/main/java/lab1/controller/TeamController.java
@@ -0,0 +1,31 @@
+package lab1.controller;
+
+import lab1.domain.Team;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TeamController {
+
+    @RequestMapping("/teams")
+    public List<Team> getTeams() {
+        List<Team> list = new ArrayList<>();
+
+        Team team = new Team();
+        team.setId(0l);
+        team.setLocation("Harlem");
+        team.setName("Globetrotters");
+        list.add(team);
+
+        team = new Team();
+        team.setId(1l);
+        team.setLocation("Washington");
+        team.setName("Generals");
+        list.add(team);
+
+        return list;
+    }
+}
---
+++ lab1-spring/src/main/java/lab1/domain/Team.java
@@ -0,0 +1,41 @@
+package lab1.domain;
+
+public class Team {
+
+    private Long id;
+    private String name;
+    private String location;
+    private String mascotte;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMascotte() {
+        return mascotte;
+    }
+
+    public void setMascotte(String mascotte) {
+        this.mascotte = mascotte;
+    }
+}

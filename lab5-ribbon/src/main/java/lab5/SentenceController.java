85c46df7180f9940f71584215bbf5bd70a2a0442
Tobias Johansson <tobias@johansson.xyz>
2017-10-01 22:06:05 +0200

    lab5-ribbon: Complete tasks

--- lab5-ribbon/src/main/java/lab5/SentenceController.java
+++ lab5-ribbon/src/main/java/lab5/SentenceController.java
@@ -1,73 +1,57 @@
 package lab5;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-
-
 @Controller
 public class SentenceController {
 
-	@Autowired DiscoveryClient client;
-	
-	
-	/**
-	 * Display a small list of Sentences to the caller:
-	 */
-	@GetMapping("/sentence")
-	public @ResponseBody String getSentence() {
-	  return 
-		"<h3>Some Sentences</h3><br/>" +	  
-		buildSentence() + "<br/><br/>" +
-		buildSentence() + "<br/><br/>" +
-		buildSentence() + "<br/><br/>" +
-		buildSentence() + "<br/><br/>" +
-		buildSentence() + "<br/><br/>"
-		;
-	}
+    @Autowired
+    RestTemplate template;
 
-	
-	/**
-	 * Assemble a sentence by gathering random words of each part of speech:
-	 */
-	public String buildSentence() {
-		String sentence = "There was a problem assembling the sentence!";
-		try{
-			sentence =  
-				String.format("%s %s %s %s %s.",
-					getWord("SUBJECT"),
-					getWord("VERB"),
-					getWord("ARTICLE"),
-					getWord("ADJECTIVE"),
-					getWord("NOUN") );			
-		} catch ( Exception e ) {
-			System.out.println(e);
-		}
-		return sentence;
-	}
+    /**
+     * Display a small list of Sentences to the caller:
+     */
+    @GetMapping("/sentence")
+    public @ResponseBody
+    String getSentence() {
+        return
+                String.format("%s\n%s\n%s\n%s\n%s\n%s",
+                        "<h3>Some Sentences</h3><br/>",
+                        buildSentence() + "<br/><br/>",
+                        buildSentence() + "<br/><br/>",
+                        buildSentence() + "<br/><br/>",
+                        buildSentence() + "<br/><br/>",
+                        buildSentence() + "<br/><br/>");
+    }
 
-	
-	/**
-	 * Obtain a random word for a given part of speech, where the part 
-	 * of speech is indicated by the given service / client ID:
-	 */
-	public String getWord(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0 ) {
-      	URI uri = list.get(0).getUri();
-	      	if (uri !=null ) {
-	      		return (new RestTemplate()).getForObject(uri,String.class);
-	      	}
+    /**
+     * Assemble a sentence by gathering random words of each part of speech:
+     */
+    public String buildSentence() {
+        String sentence = "There was a problem assembling the sentence!";
+        try {
+            sentence =
+                    String.format("%s %s %s %s %s.",
+                            getWord("SUBJECT"),
+                            getWord("VERB"),
+                            getWord("ARTICLE"),
+                            getWord("ADJECTIVE"),
+                            getWord("NOUN"));
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        return null;
-	}
+        return sentence;
+    }
 
+    /**
+     * Obtain a random word for a given part of speech, where the part
+     * of speech is indicated by the given service / client ID:
+     */
+    public String getWord(String service) {
+        return template.getForObject("http://" + service, String.class);
+    }
 }

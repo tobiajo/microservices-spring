1c18ca21655cdd5a84f9889232bd8394a1dbbdd1
Tobias Johansson <tobias@johansson.xyz>
2017-10-01 14:42:53 +0200

    lab3-cloud: Update to Boot 1.5.7

--- lab3-cloud/server/src/main/java/lab3/server/Server.java
+++ lab3-cloud/server/src/main/java/lab3/server/Server.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class Application {
+public class Server {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Server.class, args);
     }
 }

96d6891c570d780d8db7f8aa1699ca2aeaa0c7b6
Tobias Johansson <tobias@johansson.xyz>
2017-10-01 19:28:49 +0200

    Separate sentence and word client runners

--- lab4-eureka/client/run-word-clients.sh
+++ lab4-eureka/client/run-word-clients.sh
@@ -1,7 +1,5 @@
-cd client
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=subject" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=verb" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=article" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=adjective" &
-mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=noun" &
-mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=sentence"
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=noun"
